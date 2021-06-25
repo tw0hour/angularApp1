496a4a964963c5d7ad9afc4c2b5e04623ec68646
kelyan-9560 <kelyan.bervin@gmail.com>
2021-06-25 16:41:03 +0100

    'fix'

---
+++ src/app/add-food/add-food.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {FoodServices} from "../services/food.services";
+import {Food} from "../models/food.model";
+import {TypeFoodServices} from "../services/typeFood.services";
+import {TypeFood} from "../models/typeFood.model";
+
+@Component({
+  selector: 'app-add-food',
+  templateUrl: './add-food.component.html',
+  styleUrls: ['./add-food.component.scss']
+})
+export class AddFoodComponent implements OnInit {
+
+  food = new Food();
+  foodType = new TypeFood();
+  typesFood: TypeFood[] | undefined;
+
+  constructor(private foodServices: FoodServices, private typeFoodServices: TypeFoodServices) { }
+
+  ngOnInit(): void {
+    this.typeFoodServices.getAllTypeFood().subscribe(types => {
+      this.typesFood = types;
+    });
+  }
+
+  addFood(){
+    this.foodServices.addFood(this.food).subscribe();
+  }
+
+}
---
+++ src/app/medicament/medicament.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {Medicament} from "../models/medicament.model";
+import {MedicamentServices} from "../services/medicament.services";
+
+@Component({
+  selector: 'app-medicament',
+  templateUrl: './medicament.component.html',
+  styleUrls: ['./medicament.component.scss']
+})
+export class MedicamentComponent implements OnInit {
+
+  medicament = new Medicament();
+
+  constructor(private medicamentService: MedicamentServices) { }
+
+  ngOnInit(): void {
+  }
+
+  addMedicament(){
+    this.medicamentService.addMedicament(this.medicament).subscribe(medicamentAdded => {
+      console.log(medicamentAdded);
+    });
+  }
+
+  getMedicamentById(){
+    if(this.medicament.id === undefined) return
+    this.medicamentService.getMedicamentById(this.medicament.id);
+  }
+
+
+
+
+
+}
